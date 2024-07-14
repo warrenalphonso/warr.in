c402a70bfb447a9558b481728f8255b5d11594c5
Warren Alphonso <complaints@warr.in>
2024-07-14 16:16:04 -0700

    host c302-randi.html and use git lfs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./theme/templates/**/*.html", "./content/**/*.html"],
+    content: [
+        "./theme/templates/**/*.html",
+        "./content/**/*.html",
+        // Ignore uploads directory
+        "!./content/uploads/**/*.html",
+    ],
     theme: {
         extend: {},
     },
